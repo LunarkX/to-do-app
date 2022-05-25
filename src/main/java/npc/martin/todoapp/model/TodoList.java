5a83322beda01a7580bcdf41cd8564620fa9b45d
LunarkX <bikathimartin@outlook.com>
2022-05-25 12:52:39 +0300

    Added model and controller packages.

---
+++ src/main/java/npc/martin/todoapp/model/TodoList.java
@@ -0,0 +1,12 @@
+package npc.martin.todoapp.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author bikathi_martin
+ * <h3>TODO OBJECTS</h3>
+ */
+public class TodoList {
+    List<TodoObject> todoList;
+}
---
+++ src/main/java/npc/martin/todoapp/model/TodoObject.java
@@ -0,0 +1,89 @@
+package npc.martin.todoapp.model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ *
+ * @author bikathi_martin
+ */
+
+public class TodoObject {
+    private String todoId;
+    private String todoDefinition;
+    private String todoDetails;
+    private LocalDate dateCreated;
+    private LocalTime timeCreated;
+    private LocalDate dateToExecute;
+    private LocalDate dateExecuted;
+
+    public String getToDo() {
+        return todoId;
+    }
+
+    public void setToDo(String toDo) {
+        this.todoId = toDo;
+    }
+
+    public String getTodoDefinition() {
+        return todoDefinition;
+    }
+
+    public void setTodoDefinition(String todoDefinition) {
+        this.todoDefinition = todoDefinition;
+    }
+
+    public String getTodoDetails() {
+        return todoDetails;
+    }
+
+    public void setTodoDetails(String todoDetails) {
+        this.todoDetails = todoDetails;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalTime getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalTime timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public LocalDate getDateToExecute() {
+        return dateToExecute;
+    }
+
+    public void setDateToExecute(LocalDate dateToExecute) {
+        this.dateToExecute = dateToExecute;
+    }
+
+    public LocalDate getDateExecuted() {
+        return dateExecuted;
+    }
+
+    public void setDateExecuted(LocalDate dateExecuted) {
+        this.dateExecuted = dateExecuted;
+    }
+    
+    public TodoObject(String todoDefinition, String todoDetails, LocalDate dateCreated, LocalTime timeCreated, 
+            LocalDate dateToExecute, LocalDate dateExecuted) {
+        this.todoDefinition = todoDefinition;
+        this.todoDetails = todoDetails;
+        this.dateCreated = dateCreated;
+        this.timeCreated = timeCreated;
+        this.dateToExecute = dateToExecute;
+        this.dateExecuted = dateExecuted;
+    }
+    
+    public TodoObject() {
+        //parameterless constructor
+    }
+}
