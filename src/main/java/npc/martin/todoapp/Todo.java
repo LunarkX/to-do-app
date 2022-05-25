64eeee91c398acf39ebbb40c865f1b4167caf312
LunarkX <bikathimartin@outlook.com>
2022-05-25 12:53:29 +0300

    Tested the -h and -V flags on the entry class.

--- src/main/java/npc/martin/todoapp/Todo.java
+++ src/main/java/npc/martin/todoapp/Todo.java
@@ -5,17 +5,17 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 import picocli.CommandLine;
 
-@Command(name = "td", description = "Start the ToDo command line utility.")
+@Command(name = "td", description = "Start the ToDo command line utility.", version = "Todo-1.0-BETA")
 public class Todo implements Runnable {
     @Override
     public void run() {
-        System.out.println("hello world");
+        
     }
     
     public static void main(String[] args) {
         //for deployment
-        Integer exitCode = new CommandLine(new Todo()).execute(args);
-        System.exit(exitCode);
+        //Integer exitCode = new CommandLine(new Todo()).execute(args);
+        //System.exit(exitCode);
         
         //for testing
         new CommandLine(new Todo()).execute("-h");
