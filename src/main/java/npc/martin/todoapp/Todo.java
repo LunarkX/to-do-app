f0860a842b4ef84b8deb2df5adb969c326d6d12a
LunarkX <bikathimartin@outlook.com>
2022-05-30 08:23:36 +0300

    Editted wording on some command descriptions.

--- src/main/java/npc/martin/todoapp/Todo.java
+++ src/main/java/npc/martin/todoapp/Todo.java
@@ -14,33 +14,33 @@ import picocli.CommandLine.ParameterException;
 import picocli.CommandLine.Spec;
 
 @Command(name = "todo", description = "Start the Todo command line utility.", mixinStandardHelpOptions = true, 
-        version = "Todo-1.0-STABLE")
+        version = "Todo-1.0 STABLE")
 public class Todo implements Runnable {
     @Spec CommandSpec spec;
     
-    @Command(name = "new-interactive", description = "Create a new todo non-interactively.")
+    @Command(name = "new-interactive", description = "Create a new todo interactively.", mixinStandardHelpOptions = true)
     public void newTodo() {
         new CreateTodo().createATodo();
     }
     
-    @Command(name = "new", description = "Create a new todo interactively. ")
+    @Command(name = "new", description = "Create a new todo no-interactive.", mixinStandardHelpOptions = true)
     public void newTodo(@Option(names = "--definition", description = "Upto three words describing the todo.", paramLabel = "short description") String definition, 
             @Option(names = "--details", description = "A longer paragraph describing the todo.", paramLabel = "longer details") String details, 
             @Option(names = "--date", description = "Date to execute the todo. Format dd MMM yyyy e.g 21 Feb 2021.", paramLabel = "date to execute") CharSequence dateToExecute) {
         new CreateTodo().createATodo(definition, details, dateToExecute);
     }
     
-    @Command(name = "search", description = "Search for a todo by the id.")
+    @Command(name = "search", description = "Search for a todo by its id.", mixinStandardHelpOptions = true)
     void searchTodo(@Option(names = "--id", description = "The id of the todo to search for.", paramLabel = "ID") String id) {
         new FindAndEditTodo().findSpecTodo(id);
     }
     
-    @Command(name = "edit", description = "Edit data of an existing todo.")
+    @Command(name = "edit", description = "Edit an existing todo by its id.")
     void editTodo(@Option(names = "--id", description = "The id of the todo to edit.", paramLabel = "ID") String id) {
         new FindAndEditTodo().editTodo(id);
     }
     
-    @Command(name = "view", description = "View data of an existing todo.")
+    @Command(name = "view", description = "View details of an existing todo.")
     void viewTodo(@Option(names = "--id", description = "The id of the todo you want to view.", paramLabel = "ID") String id) {
         new GenerateTodoTables().generateWithID(id);
     }
@@ -57,7 +57,7 @@ public class Todo implements Runnable {
     
     public static void main(String[] args) {
         //when the code starts, we always look for the storage directory
-        File storageFolder = new File(System.getProperty("user.home") + File.separator + ".todoappdata");
+        File storageFolder = new File(System.getProperty("user.home") + File.separator + ".todoapp");
         
         //if it does exist we simply proceed running the user's commands
         if(storageFolder.exists()) {
@@ -66,18 +66,17 @@ public class Todo implements Runnable {
             System.exit(exitCode);
 
             //for testing
-            //new CommandLine(new Todo()).execute("view", "--id", "cb325677");
+            //new CommandLine(new Todo()).execute("-h");
         
         //else we create it and fill it with sample data first, then proceed executing user commands
         } else {
             new InitSample();
-            System.out.println("Created new folder coz it does not exist.");
             //for deployment
             Integer exitCode = new CommandLine(new Todo()).execute(args);
             System.exit(exitCode);
 
             //for testing
-            //new CommandLine(new Todo()).execute("");
+            //new CommandLine(new Todo()).execute("-h");
         }
     }
 }
