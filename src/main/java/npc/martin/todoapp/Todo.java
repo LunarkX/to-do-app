f247adb5c51388637d883a5fd85c6dd09a471436
LunarkX <bikathimartin@outlook.com>
2022-06-01 17:46:46 +0300

    Repackaged file with new changes to findSpecTodo() method.

--- src/main/java/npc/martin/todoapp/Todo.java
+++ src/main/java/npc/martin/todoapp/Todo.java
@@ -14,44 +14,44 @@ import picocli.CommandLine.ParameterException;
 import picocli.CommandLine.Spec;
 
 @Command(name = "todo", description = "Start the Todo command line utility.", mixinStandardHelpOptions = true, 
-        version = "Todo-1.0 STABLE")
+        version = "Todo-1.1 STABLE")
 public class Todo implements Runnable {
     @Spec CommandSpec spec;
     
     @Command(name = "new-interactive", description = "Create a new todo interactively.", mixinStandardHelpOptions = true)
-    private void newTodo() {
+    protected void newTodo() {
         new CreateTodo().createATodo();
     }
     
     @Command(name = "new", description = "Create a new todo no-interactive.", mixinStandardHelpOptions = true)
-    private void newTodo(@Option(names = "--definition", description = "Upto three words describing the todo.", paramLabel = "short description") String definition, 
+    protected void newTodo(@Option(names = "--definition", description = "Upto three words describing the todo.", paramLabel = "short description") String definition, 
             @Option(names = "--details", description = "A longer paragraph describing the todo.", paramLabel = "longer details") String details, 
             @Option(names = "--date", description = "Date to execute the todo. Format dd MMM yyyy e.g 21 Feb 2021.", paramLabel = "date to execute") CharSequence dateToExecute) {
         new CreateTodo().createATodo(definition, details, dateToExecute);
     }
     
     @Command(name = "search", description = "Search for a todo by its id.", mixinStandardHelpOptions = true)
-    private void searchTodo(@Option(names = "--id", description = "The id of the todo to search for.", paramLabel = "ID") String id) {
+    protected void searchTodo(@Option(names = "--id", description = "The id of the todo to search for.", paramLabel = "ID") String id) {
         new FindAndEditTodo().findSpecTodo(id);
     }
     
     @Command(name = "edit", description = "Edit an existing todo by its id.")
-    private void editTodo(@Option(names = "--id", description = "The id of the todo to edit.", paramLabel = "ID") String id) {
+    protected void editTodo(@Option(names = "--id", description = "The id of the todo to edit.", paramLabel = "ID") String id) {
         new FindAndEditTodo().editTodo(id);
     }
     
     @Command(name = "view", description = "View details of an existing todo.")
-    private void viewTodo(@Option(names = "--id", description = "The id of the todo you want to view.", paramLabel = "ID") String id) {
+    protected void viewTodo(@Option(names = "--id", description = "The id of the todo you want to view.", paramLabel = "ID") String id) {
         new GenerateTodoTables().generateWithID(id);
     }
     
     @Command(name = "mark-done", description = "Mark a todo as done.")
-    private void markDone(@Option(names = "--id", description = "The id of the todo to mark as done.", paramLabel = "ID") String id) {
+    protected void markDone(@Option(names = "--id", description = "The id of the todo to mark as done.", paramLabel = "ID") String id) {
         new MarkAsDone().markTodoDone(id);
     }
     
     @Command(name = "view-all", description = "Generate an overview of all todos(un-done).")
-    private void viewTodo() {
+    protected void viewTodo() {
         new GenerateTodoTables().generateWithoutID();
     }
     
@@ -67,18 +67,16 @@ public class Todo implements Runnable {
         //if it does exist we simply proceed running the user's commands
         if(storageFolder.exists()) {
             //for deployment
-            //Integer exitCode = new CommandLine(new Todo()).execute(args);
-            //System.exit(exitCode);
+            System.exit(new CommandLine(new Todo()).execute(args));
 
             //for testing
-            new CommandLine(new Todo()).execute("view-all");
+            //new CommandLine(new Todo()).execute("search", "--id", "5b7d0094");
         
         //else we create it and fill it with sample data first, then proceed executing user commands
         } else {
             new InitSample();
             //for deployment
-            Integer exitCode = new CommandLine(new Todo()).execute(args);
-            System.exit(exitCode);
+            System.exit(new CommandLine(new Todo()).execute(args));
 
             //for testing
             //new CommandLine(new Todo()).execute("-h");
