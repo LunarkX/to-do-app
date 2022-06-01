23cc380a750f2f9b5a6a08078506850b8b3aced9
LunarkX <bikathimartin@outlook.com>
2022-06-01 17:44:41 +0300

    Enhanced searching capabilities of the findSpecTodo() method.

--- src/main/java/npc/martin/todoapp/controllers/FindAndEditTodo.java
+++ src/main/java/npc/martin/todoapp/controllers/FindAndEditTodo.java
@@ -2,6 +2,8 @@ package npc.martin.todoapp.controllers;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import npc.martin.todoapp.model.TodoList;
 import npc.martin.todoapp.model.TodoObject;
 import npc.martin.todoapp.model.PersistenceTransactions;
@@ -17,6 +19,7 @@ public class FindAndEditTodo extends CreateTodo {
     //helper methods to perform transactions to and from the storage files
     private Integer positionInList = null;
     PersistenceTransactions transaction = new PersistenceTransactions();
+    private List<TodoObject> markedDone = new ArrayList<>();
     
     /**
      * <h3>findPsecTodo()</h3>
@@ -26,8 +29,9 @@ public class FindAndEditTodo extends CreateTodo {
      * @param targetTodoId 
      */
     public void findSpecTodo(String targetTodoId) {
-        //first we load the list to search from
+        //first we load the lists to search from
         listActions = transaction.readSavedJSON();
+        markedDone = transactions.readMarkedDone();
         
         //then we loop through the list
         //we compare your provided id to that of each item in the list
@@ -35,7 +39,7 @@ public class FindAndEditTodo extends CreateTodo {
             String todoItemId = todoItem.getTodoId();
             //if we find an id that matches
             if(todoItemId.equals(targetTodoId)) {
-               //we get its position in the lst and generate a table to show you what we found
+               //we get its position in the list and generate a table to show you what we found
                //the table has information linked to that id
                positionInList = listActions.getTodoList().indexOf(todoItem);
                System.out.println("Match found! Populating results table... ");
@@ -44,10 +48,37 @@ public class FindAndEditTodo extends CreateTodo {
                //we break out of the loop when we find a match
                break;
                
-            //else if we don't have a match after looping through the entire list, we print out a notification
-            } else if(!(todoItemId.equals(targetTodoId)) && 
-                    (listActions.getTodoList().indexOf(todoItem) == listActions.getTodoList().size() - 1)) {
-                System.out.println("Sorry, no match for that ID :(");
+            //else if we don't have a match after looping through the entire first list, we print out a notification
+            //the start looking in the second list
+            } else if((listActions.getTodoList().indexOf(todoItem) == listActions.getTodoList().size() - 1)) {
+                System.out.print("Sorry, no match for that ID :( \nSearching in the list of done todos...");
+                for(TodoObject itemMarkedDone : markedDone) {
+                    String itemId = itemMarkedDone.getTodoId();
+                    //if we find an id that matches
+                    if(itemId.equals(targetTodoId)) {
+                        //we get its position in the list and generate a table to show you what we found
+                       //the table has information linked to that id
+                       positionInList = markedDone.indexOf(itemMarkedDone);
+                       System.out.println("Match found! Populating results table... ");
+                       
+                       //because the available method to generate a table requires a TodoList object, we will
+                       //have to build one on the fly and pass it to the method
+                       TodoList temp = new TodoList();
+                       temp.addTodo(markedDone.get(positionInList));
+                       
+                       //we pass in '0' because the index of the item in the temp object's list
+                       //will always be 0
+                       new GenerateTodoTables().generateWithIndex(0, temp);
+                       
+                       //we break out of the loop when we find a match
+                       break;
+                       
+                       //but if we have come to the end of the second list
+                       //then we print a message saying the item has not been found on both lists
+                    } else if(markedDone.indexOf(itemMarkedDone) == markedDone.size() - 1) {
+                       System.out.print("Sorry, no match for that ID in both lists :(");
+                    }
+                }
             }
         }
     }
